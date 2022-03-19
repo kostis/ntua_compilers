e803e9bad7fef81e2d0c9ea6253ef879d91a11bc
Kostis Sagonas <kostis@it.uu.se>
2022-03-19 16:39:32 +0200

    Add generator of syntactically correct Edsger programs

---
+++ edsger/syntax_gen/edsger_pp.erl
@@ -0,0 +1,121 @@
+%%% -*- erlang-indent-level: 2 -*-
+%%% -------------------------------------------------------------------
+%%%
+%%% @doc A (not so pretty) pretty-printer of Edsger programs.
+%%%
+%%% <p>It exports three functions:
+%%%   1. pp/1, which returns an IO list of the string of a Edsger program
+%%%   2. p/0, which prints a Edsger program on stdout
+%%%   3. gen/1, which generates N programs under a `programs' directory
+%%% The first of these functions is general; the others have a hard-coded
+%%% call to the generator of syntactically correct Edsger programs.  This
+%%% needs to be made more general, at some point.</p>
+%%%
+%%% TODO: Some expressions may still need to be enclosed in parentheses.
+%%%
+%%% -------------------------------------------------------------------
+-module(edsger_pp).
+
+-export([gen/1, p/0, pp/1]).
+
+-define(F(S, L), io_lib:format(S, L)).
+
+-spec gen(non_neg_integer()) -> 'ok'.
+
+gen(0) -> ok;
+gen(N) ->
+  FN = "programs/p"++integer_to_list(N)++".eds",
+  {ok, P} = proper_gen:pick(edsger_syntax_gen:program()),
+  ok = file:write_file(FN, pp(P)),
+  gen(N - 1).
+
+-spec p() -> 'ok'.
+
+p() ->
+  {ok, P} = proper_gen:pick(edsger_syntax_gen:program()),
+  io:format(pp(P)).
+
+-spec pp(_) -> iolist().  % XXX: define a type for Edsger abstract syntax
+
+pp([]) -> "";
+pp([H|T]) -> ?F("~s~n~s", [pp(H), pp(T)]);
+pp({'vardecl', Type, Decls}) -> ?F("~s ~s;", [pp(Type), pp_decls(Decls)]);
+pp({'fundecl', RT, I,  Params}) ->
+  ?F("~s ~s(~s);", [pp(RT), I, pp_params(Params)]);
+pp({'fundef', RT, I,  Params, Decls, Stmts}) ->
+  ?F("~s ~s(~s) {~n~s~n~s}",
+     [pp(RT), I, pp_params(Params), pp(Decls), pp(Stmts)]);
+pp({'type', Type, Stars}) -> ?F("~s~s", [Type, Stars]);
+pp({'stmt', {'blck', Stmts}}) -> ?F("{~n ~s~n}", [pp(Stmts)]);
+pp({'stmt', {'if', Expr, Stmt1, Stmt2}}) ->
+  ?F("if (~s) ~s~s", [pp(Expr), pp(Stmt1), pp_opt_else(Stmt2)]);
+pp({'stmt', {'for', I, Expr1, Expr2, Expr3, Stmt}}) ->
+  ?F("~sfor (~s; ~s; ~s) ~s",
+     [pp_opt_lbl(I), pp(Expr1), pp(Expr2), pp(Expr3), pp(Stmt)]);
+pp({'stmt', Stmt}) -> ?F("~s;", [pp(Stmt)]);  % all other statements need ;
+pp({'cnt', I}) -> ?F("continue~s", [pp_opt(I)]);
+pp({'brk', I}) -> ?F("break~s", [pp_opt(I)]);
+pp({'ret', Expr}) -> ?F("return~s", [pp_opt(Expr)]);
+pp({'prn', E}) -> ?F("(~s)", [pp(E)]);
+pp({'cll', I, Es}) -> ?F("~s(~s)", [pp(I), pp_exprs(Es)]);
+pp({'arr', E1, E2}) -> ?F("~s[~s]", [pp(E1), pp(E2)]);
+pp({'uop', Op, E}) -> ?F("~s ~s", [pp(Op), pp_pe(E)]);
+pp({'bop', Op, E1, E2}) -> ?F("(~s ~s ~s)", [pp_pe(E1), pp(Op), pp_pe(E2)]);
+pp({'uas', As, E, 'pre'}) -> ?F("~s~s", [pp(As), pp_pe(E)]);
+pp({'uas', As, E, 'post'}) -> ?F("~s~s", [pp_pe(E), pp(As)]);
+pp({'bas', As, E1, E2}) -> ?F("(~s ~s ~s)", [pp_pe(E1), pp(As), pp_pe(E2)]);
+pp({'cst', Type, E}) -> ?F("(~s)~s", [pp(Type), pp_pe(E)]);
+pp({'cnd', E1, E2, E3}) ->
+  ?F("~s ? ~s : ~s", [pp_pe(E1), pp_pe(E2), pp_pe(E3)]);
+pp({'new', T, Expr}) -> ?F("new ~s~s", [pp(T), pp_opt_arrexpr(Expr)]);
+pp({'del', Expr}) -> ?F("delete ~s", [pp(Expr)]);
+pp({'string', S}) -> ?F("~p", [S]);
+pp({'char', C}) -> pp_char(C);
+pp(N) when is_number(N) -> ?F("~p", [N]);
+pp(A) when is_atom(A) -> ?F("~s", [A]).
+
+pp_exprs([E]) -> ?F("~s", [pp(E)]);
+pp_exprs([E|Es]) -> ?F("~s, ~s", [pp(E), pp_exprs(Es)]).
+
+pp_decls([D]) -> ?F("~s", [pp_decl(D)]);
+pp_decls([D|Ds]) -> ?F("~s, ~s", [pp_decl(D), pp_decls(Ds)]).
+
+pp_decl({'decl', I, Expr}) -> ?F("~s~s", [I, pp_opt_arrexpr(Expr)]).
+
+pp_opt('') -> "";
+pp_opt(X) -> ?F(" ~s", [pp(X)]).
+
+pp_opt_else('') -> "";
+pp_opt_else(Stmt) -> ?F(" else ~s", [pp(Stmt)]).
+
+pp_opt_lbl('') -> "";
+pp_opt_lbl(I) -> ?F("~s: ", [pp(I)]).
+
+pp_opt_arrexpr('') -> "";
+pp_opt_arrexpr(Expr) -> ?F("[~s]", [pp(Expr)]).
+
+%% pp_params([]) -> "";
+pp_params([P]) -> ?F("~s", [pp_param(P)]);
+pp_params([P1|Ps]) -> ?F("~s, ~s", [pp_param(P1), pp_params(Ps)]).
+
+pp_param({'param', P, Type, I}) -> ?F("~s~s ~s", [P, pp(Type), I]).
+
+
+%%
+%% Exists only for determining when to put parentheses around an expression.
+%% Simple expressions are not parenthesized.
+%%
+pp_pe({'string', _}=E) -> pp(E);
+pp_pe({'char', _}=E) -> pp(E);
+pp_pe(N) when is_number(N) -> pp(N);
+pp_pe(A) when is_atom(A) -> pp(A);
+pp_pe(E) -> ?F("(~s)", [pp(E)]).
+
+%%
+%% The pretty-printing of chars is hard-coded
+%% (due to Erlang printing chars differently),
+%%
+pp_char('a') -> "'a'";
+pp_char('7') -> "'7'";
+pp_char('\n') -> "'\\n'";
+pp_char('\'') -> "'\\''".
---
+++ edsger/syntax_gen/edsger_syntax_gen.erl
@@ -0,0 +1,150 @@
+%%% -*- erlang-indent-level: 2 -*-
+%%% -------------------------------------------------------------------
+%%% 
+%%% @doc A random generator of Abstract Syntax Trees of syntactically
+%%%      valid Edsger programs.
+%%%
+%%% Generate the AST of a random program by:
+%%%    proper_gen:pick(edsger_syntax_gen:program()).
+%%% and repeat issuing this call as many times as you like.
+%%%
+%%% Alternatively, pretty-print a random Edsger program on stdout by:
+%%%    edsger_pp:p().
+%%% and repeat this until you are satisfied (or bored).
+%%% 
+%%% -------------------------------------------------------------------
+-module(edsger_syntax_gen).
+
+-export([program/0]).
+
+-include_lib("proper/include/proper.hrl").
+
+prop_gen() ->
+  ?FORALL(P, program(), erlang:display(P)).
+
+%%
+%% Some convenient utilities
+%%
+-define(OPT(GEN), union(['', GEN])).
+-define(STAR(MAX, GEN), ?LET(N, integer(0, MAX), vector(N, GEN))).
+-define(PLUS(MAX, GEN), ?LET(N, integer(1, MAX), vector(N, GEN))).
+
+%%
+%% Arbitrary limits
+%%
+-define(MAX_DECLS,   5).
+-define(MAX_PARAMS,  4).
+-define(MAX_STMTS,   5).
+
+program() ->
+  M = 3 + rand:uniform(8),  % M ranges in 4..11
+  frequency([{1, vector(1, decl())},
+	     {2, vector(2, decl())},
+	     {3, vector(3, decl())},
+	     {4, vector(M, decl())}]).
+
+decl() ->
+  frequency([{1, var_decl()}, {1, fun_decl()}, {3, fun_def()}]).
+
+var_decl() ->
+  {'vardecl', type(), declarators()}.
+
+type() ->
+  {'type', basic_type(), stars()}.
+
+basic_type() ->
+  union(['int', 'char', 'bool', 'double']).
+
+stars() ->
+  frequency([{6,''}, {4,'*'}, {2,'**'}, {1,'***'}]).
+
+declarators() ->
+  ?PLUS(?MAX_DECLS, declarator()).
+
+declarator() ->
+  frequency([{9, {'decl','I'(),''}}, {1, {'decl','I'(),const_expr()}}]).
+
+fun_decl() ->
+  {'fundecl', res_type(), 'I'(), param_list()}.
+
+res_type() ->
+  frequency([{5, type()}, {1, {'type','void',''}}]).
+
+param_list() ->
+  ?PLUS(?MAX_PARAMS, param()).
+
+param() ->
+  {'param', ?OPT('byref '), type(), 'I'()}.
+
+fun_def() ->
+  {'fundef', res_type(), 'I'(), param_list(),
+   ?STAR(?MAX_DECLS div 2, decl()), ?STAR(?MAX_STMTS, stmt())}.
+
+stmt() ->
+  ?SIZED(Size, stmt(Size)).
+
+stmt(Sz) ->
+  Sz2 = Sz div 2,
+  S = union(['', expr(Sz),
+	     {'blck', ?STAR(?MAX_STMTS div 2, ?LAZY(stmt(Sz2)))},
+	     {'if', expr(1), ?LAZY(stmt(Sz2)), ?OPT(?LAZY(stmt(Sz2)))},
+	     {'for', ?OPT('I'()), ?OPT(expr(1)), ?OPT(expr(1)), ?OPT(expr(1)),
+	      ?LAZY(stmt(Sz2))},
+	     {'cnt', ?OPT('I'())}, {'brk', ?OPT('I'())},
+	     {'ret', ?OPT(expr(1))}]),
+  {'stmt', S}.
+
+expr(Size) when Size =< 1 ->
+  union(['I'(), 'true', 'false', 'NULL',
+	 int_const(), char_const(), double_const(), string_literal()]);
+expr(Sz) ->
+  Sz1 = Sz - 1, Sz2 = Sz div 2, Sz4 = Sz div 4,
+  union([{'prn', ?LAZY(expr(Sz1))},
+	 {'cll', 'I'(), exprs(Sz1, 1, ?MAX_PARAMS)},
+	 {'arr', ?LAZY(expr(Sz4)), ?LAZY(expr(Sz4))},
+	 {'uop', un_op(), ?LAZY(expr(Sz1))},
+	 {'bop', bin_op(), ?LAZY(expr(Sz2)), ?LAZY(expr(Sz2))},
+	 {'uas', un_asgn(), ?LAZY(expr(Sz4)), oneof(['pre', 'post'])},
+	 {'bas', bin_asgn(), ?LAZY(expr(Sz2)), ?LAZY(expr(Sz2))},
+	 {'cst', type(), ?LAZY(expr(Sz1))},
+	 {'cnd', ?LAZY(expr(Sz2)), ?LAZY(expr(Sz2)), ?LAZY(expr(Sz2))},
+	 {'new', type(), ?OPT(?LAZY(expr(Sz2)))},
+ 	 {'del', ?LAZY(expr(Sz2))}]).
+
+exprs(Sz, Min, Max) ->
+  ?LET(N, integer(Min, Max), vector(N, expr(Sz))).
+
+const_expr() ->
+  expr(1).
+
+un_op() ->
+  union(['&', '*', '+', '-', '!']).
+
+bin_op() ->
+  union(['*', '/', '%', '+', '-', '<', '>', '<=', '>=', '==', '!=',
+	 '&&', '||', ',']).
+
+un_asgn() ->
+  union(['++', '--']).
+
+bin_asgn() ->
+  union(['=', '*=', '/=', '%=', '+=', '-=']).
+
+%% Lexical terminals below -- they can be extended at will
+
+'I'() ->
+  union(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+	 'x', 'y', 'z', 'foo', 'bar', 'baz', 'boo', 'main']).
+
+int_const() ->
+  integer(0, 42).
+
+double_const() ->
+  union([0.0, 2.56, 3.14, 0.420e+2, 42000.0e-3]).
+
+char_const() ->  % These are special-cased in edsger_pp!
+  {'char', union(['a', '7', '\n', '\''])}.
+
+string_literal() ->
+  {'string', union(["foo", "bar", "Route66",
+		    "Name:\t\"DouglasAdams\"\nValue:\t42\n"])}.
