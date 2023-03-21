c587af7d50fb1529c0bf30b3d7e45366f92b8ea1
Kostis Sagonas <kostis@cs.ntua.gr>
2023-03-21 15:53:15 +0100

    Add syntactically correct generator of Grace programs (#10)
    * Initial version of Grace syntax_gen programs
    
    * Fix some Edsger leftovers
    
    * Add syntax generator for Grace programs

---
+++ grace/syntax_gen/grace_pp.erl
@@ -0,0 +1,127 @@
+%%% -*- erlang-indent-level: 2 -*-
+%%% -------------------------------------------------------------------
+%%%
+%%% @doc A (not so pretty) pretty-printer of Grace programs.
+%%%
+%%% <p>It exports three functions:
+%%%   1. pp/1, which returns an IO list of the string of a Grace program
+%%%   2. p/0, which prints a Grace program on stdout
+%%%   3. gen/1, which generates N programs under a `programs' directory
+%%% The first of these functions is general; the others have a hard-coded
+%%% call to the generator of syntactically correct Grace programs.  This
+%%% needs to be made more general, at some point.</p>
+%%%
+%%% TODO: Some expressions may still need to be enclosed in parentheses.
+%%%
+%%% -------------------------------------------------------------------
+-module(grace_pp).
+
+-export([gen/1, p/0, pp/1]).
+
+-define(F(S, L), io_lib:format(S, L)).
+
+-spec gen(non_neg_integer()) -> 'ok'.
+
+gen(0) -> ok;
+gen(N) ->
+  FN = "programs/p"++integer_to_list(N)++".grc",
+  {ok, P} = proper_gen:pick(grace_syntax_gen:program()),
+  ok = file:write_file(FN, pp(P)),
+  gen(N - 1).
+
+-spec p() -> 'ok'.
+
+p() ->
+  {ok, P} = proper_gen:pick(grace_syntax_gen:program()),
+  io:format(pp(P)).
+
+-spec pp(_) -> iolist().  % XXX: define a type for Grace abstract syntax
+
+pp([]) -> "";
+pp([H|T]) -> ?F("~s~n~s", [pp(H), pp(T)]);
+%% fun stuff
+pp({'fun', I,  FParDefs, RT}) ->
+  ?F("fun ~s(~s) : ~s", [I, pp_fpars(FParDefs), pp(RT)]);
+pp({'fundef', Header,  LocalDefs, Block}) ->
+  ?F("~s~n~s~n~s~n", [pp(Header), pp(LocalDefs), pp(Block)]);
+pp({'fundecl', Header}) -> ?F("~s;", [pp(Header)]);
+pp({'type', Type, Brackets}) -> ?F("~s~s", [Type, pp_brackets(Brackets)]);
+pp({'fpar_type', Type, Opt, Brackets}) ->
+  ?F("~s~s~s", [Type, Opt, pp_brackets(Brackets)]);
+%% statements
+pp({'blck', Stmts}) -> ?F("{~n~s~n}", [pp_block(Stmts)]);
+pp({'stmt', {'if', Cond, Stmt1, Stmt2}}) ->
+  ?F("if ~s then ~s~s", [pp(Cond), pp(Stmt1), pp_opt_else(Stmt2)]);
+pp({'stmt', {'while', Cond, Stmt}}) ->
+  ?F("while ~s do ~s", [pp(Cond), pp(Stmt)]);
+pp({'stmt', Stmt}) -> ?F("~s;", [pp(Stmt)]);  % all other statements need ;
+pp({'<-', LVal, Expr}) -> ?F("~s <- ~s", [pp(LVal), pp(Expr)]);
+pp({'ret', Expr}) -> ?F("return~s", [pp_opt(Expr)]);
+%% conds
+pp({'not', Cond}) -> ?F("not ~s", [pp(Cond)]);
+pp({'and', Cond1, Cond2}) -> ?F("~s and ~s", [pp(Cond1), pp(Cond2)]);
+pp({'or',  Cond1, Cond2}) -> ?F("~s or ~s", [pp(Cond1), pp(Cond2)]);
+pp({'cmp', CompOp, E1, E2}) -> ?F("~s ~s ~s", [pp(E1), pp(CompOp), pp(E2)]);
+%% exprs
+pp({'uop', UnOp, E}) -> ?F("~s ~s", [pp(UnOp), pp_pe(E)]);
+pp({'bop', BOp, E1, E2}) -> ?F("(~s ~s ~s)", [pp_pe(E1), pp(BOp), pp_pe(E2)]);
+%% misc
+pp({'prn', E}) -> ?F("(~s)", [pp(E)]);
+pp({'lval', LVal, Expr}) -> ?F("~s[~s]", [pp(LVal), pp(Expr)]);
+pp({'cll', ID, Es}) -> ?F("~s(~s)", [pp(ID), pp_exprs(Es)]);
+%% basic elements
+pp({'var', IDs, Type}) -> ?F("var ~s : ~s;", [pp_ids(IDs), pp(Type)]);
+pp({'string', S}) -> ?F("~p", [S]);
+pp({'char', C}) -> pp_char(C);
+pp(I) when is_integer(I) -> ?F("~p", [I]);
+pp(A) when is_atom(A) -> ?F("~s", [A]).
+
+pp_block([]) -> "";
+pp_block([S]) -> ?F("  ~s", [pp(S)]);
+pp_block([S|Ss]) -> ?F("~s~n~s", [pp_block([S]), pp_block(Ss)]).
+
+pp_brackets('') -> "";
+pp_brackets(I) when is_integer(I) -> ?F("[~p]", [I]);
+pp_brackets({I1, I2}) -> ?F("[~p][~p]", [I1, I2]).
+
+pp_ids([ID]) -> ?F("~s", [pp(ID)]);
+pp_ids([ID|IDs]) -> ?F("~s, ~s", [pp(ID), pp_ids(IDs)]).
+
+pp_exprs([]) -> "";
+pp_exprs([E]) -> ?F("~s", [pp(E)]);
+pp_exprs([E|Es]) -> ?F("~s, ~s", [pp(E), pp_exprs(Es)]).
+
+pp_opt('') -> "";
+pp_opt(X) -> ?F(" ~s", [pp(X)]).
+
+pp_opt_else('') -> "";
+pp_opt_else(Stmt) -> ?F(" else ~s", [pp(Stmt)]).
+
+pp_opt_ref('') -> "";
+pp_opt_ref(ref) -> ?F("ref ", []).
+
+pp_fpars([FP]) -> ?F("~s", [pp_fpar(FP)]);
+pp_fpars([FP1|FPs]) -> ?F("~s, ~s", [pp_fpar(FP1), pp_fpars(FPs)]).
+
+pp_fpar({'fpar_def', Ref, IDs, Type}) ->
+  ?F("~s~s : ~s", [pp_opt_ref(Ref), pp_ids(IDs), pp(Type)]).
+
+
+%%
+%% Exists only for determining when to put parentheses around an expression.
+%% Simple expressions are not parenthesized.
+%%
+pp_pe({'string', _}=E) -> pp(E);
+pp_pe({'char', _}=E) -> pp(E);
+pp_pe(I) when is_integer(I) -> pp(I);
+pp_pe(A) when is_atom(A) -> pp(A);
+pp_pe(E) -> ?F("(~s)", [pp(E)]).
+
+%%
+%% The pretty-printing of chars is hard-coded
+%% (due to Erlang printing chars differently),
+%%
+pp_char('a') -> "'a'";
+pp_char('7') -> "'7'";
+pp_char('\n') -> "'\\n'";
+pp_char('\'') -> "'\\''".
---
+++ grace/syntax_gen/grace_syntax_gen.erl
@@ -0,0 +1,148 @@
+%%% -*- erlang-indent-level: 2 -*-
+%%% -------------------------------------------------------------------
+%%% 
+%%% @doc A random generator of Abstract Syntax Trees of syntactically
+%%%      valid Grace programs.
+%%%
+%%% Generate the AST of a random program by:
+%%%    proper_gen:pick(grace_syntax_gen:program()).
+%%% and repeat issuing this call as many times as you like.
+%%%
+%%% Alternatively, pretty-print a random Grace program on stdout by:
+%%%    grace_pp:p().
+%%% and repeat this until you are satisfied (or bored).
+%%% 
+%%% -------------------------------------------------------------------
+-module(grace_syntax_gen).
+
+-export([program/0]).
+
+-include_lib("proper/include/proper.hrl").
+
+prop_gen() ->
+  ?FORALL(P, program(), erlang:display(P)).
+
+%%
+%% Some convenient utilities
+%%
+-define(OPT(GEN), union(['', GEN])).
+-define(STAR(MAX, GEN), ?LET(N, integer(0, MAX), vector(N, GEN))).
+-define(PLUS(MAX, GEN), ?LET(N, integer(1, MAX), vector(N, GEN))).
+
+%%
+%% Arbitrary limits
+%%
+-define(MAX_DECLS,   5).
+-define(MAX_PARAMS,  3).
+-define(MAX_STMTS,   5).
+
+program() ->
+  func_def().
+
+func_def() ->
+  {'fundef', header(), ?STAR(?MAX_PARAMS, local_def()), block()}.
+
+header() ->
+  {'fun', 'I'(), ?PLUS(?MAX_DECLS, fpar_def()), ret_type()}.
+
+fpar_def() ->
+  {'fpar_def', ?OPT('ref'), ?PLUS(?MAX_PARAMS, 'I'()), fpar_type()}.
+
+type() ->
+  {'type', data_type(), brackets()}.
+
+data_type() ->
+  union(['int', 'char']).
+
+brackets() ->
+  frequency([{6, ''}, {3, int_const()}, {1, {int_const(), int_const()}}]).
+
+ret_type() ->
+  union([data_type(), 'nothing']).
+
+fpar_type() ->
+  {'fpar_type', data_type(), ?OPT('[]'), brackets()}.
+
+local_def() ->
+  frequency([{3, func_def()}, {1, func_decl()}, {1, var_def()}]).
+
+func_decl() ->
+  {'fundecl', header()}.
+
+var_def() ->
+  {'var', ?PLUS(3, 'I'()), type()}.
+
+%% stmt() ->
+%%   ?SIZED(Size, stmt(Size)).
+
+stmt(Sz) ->
+  Sz2 = Sz div 2, Sz4 = Sz div 4,
+  S = union(['',
+	     {'<-', ?LAZY(lvalue(Sz4)), ?LAZY(expr(Sz2))},
+	     block(),
+	     func_call(Sz),
+	     {'if', ?LAZY(cnd(Sz4)), ?LAZY(stmt(Sz2)), ?OPT(?LAZY(stmt(Sz2)))},
+	     {'while', ?LAZY(cnd(Sz4)), ?LAZY(stmt(Sz2))},
+	     {'ret', ?OPT(expr(Sz2))}]),
+  {'stmt', S}.
+
+block() ->
+  ?SIZED(Size, block(Size)).
+
+block(Sz) ->
+  Sz2 = Sz div 2,
+  {'blck', ?STAR(?MAX_STMTS, ?LAZY(stmt(Sz2)))}.
+
+func_call(Sz) ->
+  {'cll', 'I'(), ?STAR(?MAX_PARAMS, ?LAZY(expr(Sz)))}.  % XXX: 1
+
+lvalue(Sz) ->
+  Sz2 = Sz div 2,
+  frequency([{5, 'I'()},
+	     {1, string_literal()},
+	     {1, {lval, ?LAZY(lvalue(Sz2)), ?LAZY(expr(Sz2))}}]). 
+
+expr(Size) when Size =< 1 ->
+  union([int_const(), char_const()]);
+expr(Sz) ->
+  Sz1 = Sz - 1, Sz2 = Sz div 2,
+  union([{'prn', ?LAZY(expr(Sz2))},
+	 ?LAZY(lvalue(Sz)),
+	 func_call(Sz),
+	 {'uop', un_op(), ?LAZY(expr(Sz1))},
+	 {'bop', bin_op(), ?LAZY(expr(Sz2)), ?LAZY(expr(Sz2))}]).
+
+cnd(Sz) when Sz =< 1 -> % Base case without self-recursion
+  {'cmp', cmp_op(), expr(Sz), expr(Sz)};
+cnd(Sz) ->
+  Sz2 = Sz div 2,
+  union([{'prn', ?LAZY(cnd(Sz2))},
+	 {'not', ?LAZY(cnd(Sz2))},
+	 {'and', ?LAZY(cnd(Sz2)), ?LAZY(cnd(Sz2))},
+	 {'or',  ?LAZY(cnd(Sz2)), ?LAZY(cnd(Sz2))},
+	 {'cmp', cmp_op(), ?LAZY(expr(Sz2)), ?LAZY(expr(Sz2))}]).
+
+un_op() ->
+  union(['+', '-']).
+
+bin_op() ->
+  union(['+', '-',  '*', 'div', 'mod']).
+
+cmp_op() ->
+  union(['=', '#', '<', '>', '<=', '>=']).
+
+%% Lexical terminals below -- they can be extended at will
+
+'I'() ->
+  union(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+	 'x', 'y', 'z', 'foo', 'bar', 'baz', 'boo', 'main']).
+
+int_const() ->
+  integer(0, 42).
+
+char_const() ->  % These are special-cased in grace_pp!
+  {'char', union(['a', '7', '\n', '\''])}.
+
+string_literal() ->
+  {'string', union(["foo", "bar", "Route66",
+		    "Name:\t\"DouglasAdams\"\nValue:\t42\n"])}.
